f4658f28d86bf879c62a6fc51ed2fdb04400d037
Seth Broweleit <getsetbro@users.noreply.github.com>
2017-07-24 12:40:18 -0400

    🎛v2

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
 var vm = new Vue({
-    el:'#app',
-    data:{
-        p1:false,
-        p2:false,
-        p3:false,
-        p4:false,
-        p5:false,
-        p6:false,
-        p7:false,
-        p8:false
+    el: '#app',
+    data: {
+        p1: false,
+        p2: false,
+        p3: false,
+        p4: false,
+        p5: false,
+        p6: false,
+        p7: false,
+        p8: false
     },
-    computed:{
-        group1:function(){
-            if(this.p1 && this.p2 && this.p3){
+    computed: {
+        group1: function () {
+            if (this.p1 && this.p2 && this.p3) {
                 return true;
             }
             return false;
         },
-        group2:function(){
-            if(this.p4 && this.p5 && this.p6){
+        group2: function () {
+            if (this.p4 && this.p5 && this.p6) {
                 return true;
             }
             return false;
         },
-        group3:function(){
-            if(this.p7 && this.p8){
+        group3: function () {
+            if (this.p7 && this.p8) {
                 return true;
             }
             return false;
         }
     },
-    methods:{
-        group1click:function(){
-            if(this.p1 && this.p2 && this.p3){
+    methods: {
+        group1click: function () {
+            if (this.p1 && this.p2 && this.p3) {
                 this.p1 = false;
                 this.p2 = false;
                 this.p3 = false;
-            }else{
+            } else {
                 this.p1 = true;
                 this.p2 = true;
                 this.p3 = true;
             }
         },
-        group2click:function(){
-            if(this.p4 && this.p5 && this.p6){
+        group2click: function () {
+            if (this.p4 && this.p5 && this.p6) {
                 this.p4 = false;
                 this.p5 = false;
                 this.p6 = false;
-            }else{
+            } else {
                 this.p4 = true;
                 this.p5 = true;
                 this.p6 = true;
-            }            
+            }
         },
-        group3click:function(){
-            if(this.p7 && this.p8){
+        group3click: function () {
+            if (this.p7 && this.p8) {
                 this.p7 = false;
                 this.p8 = false;
-            }else{
+            } else {
                 this.p7 = true;
                 this.p8 = true;
-            }            
+            }
         },
     }
 });
